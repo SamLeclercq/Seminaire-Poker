71f6456a4ef857beee9b1a96e359fb385a36c2e2
baptiste <baptiste.fournel59@outlook.fr>
2026-03-25 10:43:46 +0100

    feat: enhance PlayerHandView with card node creation and resizing methods

--- frontend/src/main/java/module-info.java
+++ frontend/src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.seminairepoker.frontend {
     requires javafx.controls;
 
     exports com.seminairepoker.frontend.app;
+    exports com.seminairepoker.frontend.infrastructure.assets;
     exports com.seminairepoker.frontend.presentation.state;
     exports com.seminairepoker.frontend.presentation.view;
 }
